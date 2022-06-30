ca8c560607a94b65e164ee827d560a05e3c2df7a
Sophia Davis <sophiadavis@Sophias-MacBook-Pro.local>
2022-06-30 15:05:38 -0400

    added schemas folder with index.js and resolvers

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/googlebooks';
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/googlebooks', {
+ 
 });
 
-module.exports = mongoose.connection;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
