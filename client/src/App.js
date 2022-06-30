51c650848aee56ad8801a0b1eef0d8818b77ab4f
Sophia Davis <sophiadavis@Sophias-MacBook-Pro.local>
2022-06-30 14:37:39 -0400

    imported apollo provider, graphQL endpoint, and middleware to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,42 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+
+// import Apollo Provider
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+//  main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// middleware that will attach the JWT token 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// `authLink` middleware
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
 function App() {
   return (
+<ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +50,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
